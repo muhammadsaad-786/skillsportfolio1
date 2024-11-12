3476a8d93f6dcf5224f0e01b1706d1a1856f3255
muhammadsaad-786 <m.saad619321@gmail.com>
2024-11-12 21:00:01 +0400

    Exercise 5 Completed

---
+++ EXERCISE 5/ex5.py
@@ -0,0 +1,33 @@
+# Making a dictionary to track month names to number of days  
+months_days = {  
+  "January": 31,  
+  "February": 28,  
+  "March": 31,  
+  "April": 30,  
+  "May": 31,  
+  "June": 30,  
+  "July": 31,  
+  "August": 31,  
+  "September": 30,  
+  "October": 31,  
+  "November": 30,  
+  "December": 31  
+}  
+  
+# Ask the player to fill in the name of the month  
+month = input("Heyyy!! Enter the name of the month (e.g. January, February, etc.): ")  
+  
+# Check if the player entered correct input  
+if month in months_days:  
+  # Check if the month is feb & whether it is a leap year or no 
+  if month == "February":  
+    is_leap_year = input("Ouuu! Is it a leap Year? (Yes/No)")  
+    if is_leap_year.lower() == "yes":  
+        months_days["February"] = 29  
+    else:  
+        months_days["February"] = 28  
+
+  # Print the num of days in corresponding month  
+  print(f"There are {months_days[month]} days in {month}.")  
+else:  
+  print("Nuh-uh, wrong month name dude.")
